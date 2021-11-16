0a6d316d0b59fcf2a5b3d2d93357478ac403a060
andre <andre@DESKTOP-30RC15L>
2021-11-17 02:23:08 +0300

    4 lab done all works fine

---
+++ WEB_953502_Chaplinskiy/Controllers/Image.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WEB_953502_Chaplinskiy.Entities;
+
+namespace WEB_953502_Chaplinskiy.Controllers
+{
+    public class Image : Controller
+    {
+        UserManager<ApplicationUser> _userManager;
+        IWebHostEnvironment _env;
+
+        public Image(UserManager<ApplicationUser>
+userManager, IWebHostEnvironment env)
+        {
+            _userManager = userManager;
+            _env = env;
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        public async Task<FileResult> GetAvatar()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user.AvatarImage != null)
+                return File(user.AvatarImage, "image/...");
+            else
+            {
+                var avatarPath = "/Images/Man.png";
+                return File(_env.WebRootFileProvider
+                .GetFileInfo(avatarPath)
+
+                .CreateReadStream(), "image/...");
+
+            }
+        }
+    }
+}
--- WEB_953502_Chaplinskiy/Migrations/20211116230153_Avatar.Designer.cs
+++ WEB_953502_Chaplinskiy/Migrations/20211116230153_Avatar.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WEB_953502_Chaplinskiy.Data;
 
 namespace WEB_953502_Chaplinskiy.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211116230153_Avatar")]
+    partial class Avatar
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
