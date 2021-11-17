6c01eccc3c911487174bd4c793759a11aa6cc966
andre <andre@DESKTOP-30RC15L>
2021-11-17 19:35:49 +0300

    6 done all work fine

--- WEB_953502_Chaplinskiy/Controllers/ProductController.cs
+++ WEB_953502_Chaplinskiy/Controllers/ProductController.cs
@@ -3,68 +3,80 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WEB_953502_Chaplinskiy.Data;
 using WEB_953502_Chaplinskiy.Entities;
+using WEB_953502_Chaplinskiy.Extensions;
 using WEB_953502_Chaplinskiy.Models;
 
 namespace WEB_953502_Chaplinskiy.Controllers
 {
     public class ProductController : Controller
     {
-        List<Dish> _dishes;
-        List<DishGroup> _dishGroups;
+        ApplicationDbContext _context;
+        //List<Dish> _dishes;
+        //List<DishGroup> _dishGroups;
         int _pageSize;
-        public ProductController()
+        public ProductController(ApplicationDbContext context)
         {
             _pageSize = 3;
-            SetupData();
+            //SetupData();
+            _context = context;
         }
+        [Route("Catalog")]
+        [Route("Catalog/Page_{pageNo}")]
         public IActionResult Index(int? group, int pageNo = 1)
         {
-            var dishesFiltered = _dishes
+            var dishesFiltered = _context.Dishes
             .Where(d => !group.HasValue || d.DishGroupId == group.Value);
             // Поместить список групп во ViewData
-            ViewData["Groups"] = _dishGroups;
+            ViewData["Groups"] = _context.DishGroups;
             // Получить id текущей группы и поместить в TempData
             ViewData["CurrentGroup"] = group ?? 0;
-            return View(ListViewModel<Dish>.GetModel(dishesFiltered, pageNo,
-            _pageSize));
+            //return View(ListViewModel<Dish>.GetModel(dishesFiltered, pageNo,
+            //_pageSize));
+            var model = ListViewModel<Dish>.GetModel(dishesFiltered, pageNo,
+_pageSize);
+            if (Request.IsAjaxRequest())
+                return PartialView("_listpartial", model);
+            else
+                return View(model);
         }
         
         
         /// <summary>
         /// Инициализация списков
         /// </summary>
-        private void SetupData()
-        {
-            _dishGroups = new List<DishGroup>
-            {
-            new DishGroup {DishGroupId=1, GroupName="Стартеры"},
-            new DishGroup {DishGroupId=2, GroupName="Салаты"},
-            new DishGroup {DishGroupId=3, GroupName="Супы"},
-            new DishGroup {DishGroupId=4, GroupName="Основныеблюда"},
-            new DishGroup {DishGroupId=5, GroupName="Напитки"},
-            new DishGroup {DishGroupId=6, GroupName="Десерты"}
-            };
-            _dishes = new List<Dish>
-            {
-            new Dish {DishId = 1, DishName="Суп-харчо",
-            Description="Очень острый, невкусный",
-            Calories =200, DishGroupId=3, Image="dan.jfif" },
-            new Dish { DishId = 2, DishName="Борщ",
-            Description="Много сала, без сметаны",
-            Calories =330, DishGroupId=3, Image="dannie.jfif" },
-            new Dish { DishId = 3, DishName="Котлета пожарская",
-            Description="Хлеб - 80%, Морковь - 20%",
-            Calories =635, DishGroupId=4, Image="donnie.jpg" },
-            new Dish { DishId = 4, DishName="Макароны по-флотски",
-            Description="С охотничьей колбаской",
-            Calories =524, DishGroupId=4, Image="Man.png" },
-            new Dish { DishId = 5, DishName="Компот",
-            Description="Быстро растворимый, 2 литра",
-            Calories =180, DishGroupId=5, Image="ыру.jpg" }
+        //private void SetupData()
+        //{
+        //    _dishGroups = new List<DishGroup>
+        //    {
+        //    new DishGroup {DishGroupId=1, GroupName="Стартеры"},
+        //    new DishGroup {DishGroupId=2, GroupName="Салаты"},
+        //    new DishGroup {DishGroupId=3, GroupName="Супы"},
+        //    new DishGroup {DishGroupId=4, GroupName="Основныеблюда"},
+        //    new DishGroup {DishGroupId=5, GroupName="Напитки"},
+        //    new DishGroup {DishGroupId=6, GroupName="Десерты"}
+        //    };
+        //    _dishes = new List<Dish>
+        //    {
+        //    new Dish {DishId = 1, DishName="Суп-харчо",
+        //    Description="Очень острый, невкусный",
+        //    Calories =200, DishGroupId=3, Image="dan.jfif" },
+        //    new Dish { DishId = 2, DishName="Борщ",
+        //    Description="Много сала, без сметаны",
+        //    Calories =330, DishGroupId=3, Image="dannie.jfif" },
+        //    new Dish { DishId = 3, DishName="Котлета пожарская",
+        //    Description="Хлеб - 80%, Морковь - 20%",
+        //    Calories =635, DishGroupId=4, Image="donnie.jpg" },
+        //    new Dish { DishId = 4, DishName="Макароны по-флотски",
+        //    Description="С охотничьей колбаской",
+        //    Calories =524, DishGroupId=4, Image="Man.png" },
+        //    new Dish { DishId = 5, DishName="Компот",
+        //    Description="Быстро растворимый, 2 литра",
+        //    Calories =180, DishGroupId=5, Image="ыру.jpg" }
 
-            };
+        //    };
 
-        }
+        //}
     }
 }
--- WEB_953502_Chaplinskiy/Data/DbInitializer.cs
+++ WEB_953502_Chaplinskiy/Data/DbInitializer.cs
@@ -9,11 +9,13 @@ namespace WEB_953502_Chaplinskiy.Data
 {
     public class DbInitializer
     {
+
         public static async Task Seed(ApplicationDbContext context,
 UserManager<ApplicationUser> userManager,
 RoleManager<IdentityRole> roleManager)
 
         {
+
             // создать БД, если она еще не создана
             context.Database.EnsureCreated();
             // проверка наличия ролей
@@ -49,6 +51,50 @@ RoleManager<IdentityRole> roleManager)
                 admin = await userManager.FindByEmailAsync("admin@mail.ru");
                 await userManager.AddToRoleAsync(admin, "admin");
             }
+
+
+            if (!context.DishGroups.Any())
+            {
+                context.DishGroups.AddRange(
+                new List<DishGroup>
+                {
+new DishGroup {GroupName="Стартеры"},
+new DishGroup {GroupName="Салаты"},
+new DishGroup {GroupName="Супы"},
+new DishGroup {GroupName="Основные блюда"},
+new DishGroup {GroupName="Напитки"},
+new DishGroup {GroupName="Десерты"}
+                });
+                await context.SaveChangesAsync();
+
+            }
+            // проверка наличия объектов
+            if (!context.Dishes.Any())
+            {
+                context.Dishes.AddRange(
+                new List<Dish>
+                {
+new Dish {DishName="Суп-харчо",
+Description="Очень острый, невкусный",
+Calories =200, DishGroupId=3, Image="dan.jfif" },
+new Dish {DishName="Борщ",
+Description="Много сала, без сметаны",
+Calories =330, DishGroupId=3, Image="dannie.jfif" },
+new Dish {DishName="Котлета пожарская",
+Description="Хлеб - 80%, Морковь - 20%",
+Calories =635, DishGroupId=4, Image="donnie.jpg" },
+new Dish {DishName="Макароны по-флотски",
+Description="С охотничьей колбаской",
+Calories =524, DishGroupId=4, Image="Man.png" },
+new Dish {DishName="Компот",
+Description="Быстро растворимый, 2 литра",
+
+Calories =180, DishGroupId=5, Image="ыру.jpg" }
+                });
+                await context.SaveChangesAsync();
+            }
+
+
         }
     }
 }
